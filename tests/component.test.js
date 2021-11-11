f742d82617c017cb699430f2228d011125797b6f
garza-utsa <john.garza@utsa.edu>
2021-11-10 21:23:03 -0600

    reorganizing
    using the components folder for gspec

--- tests/component.test.js
+++ tests/component.test.js
@@ -4,5 +4,5 @@ load("pages/component.js");
 testOnAllDevices("Component page", "/", function (driver, device) {
     console.log(driver);
     new Component(driver).waitForIt();
-    checkLayout(driver, "specs/component.gspec", device.tags);
+    checkLayout(driver, "specs/components/component.gspec", device.tags);
 });
